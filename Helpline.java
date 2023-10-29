da030fdebed5e67e268d0477bcd5cb3edf2bba25
NishaDev07 <143122773+NishaDev07@users.noreply.github.com>
2023-10-29 16:05:52 +0530

    Helpline and Guidelines Integration Unit
    Upload of Both the Units: Helpline and Gudlines Reference.

--- Helpline.java
+++ Helpline.java
@@ -6,36 +6,66 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class Helpline {
+public class Helpline implements ActionListener {
+
+     JButton callButton = new JButton("Call Helpline");
+     JButton portalButton = new JButton("Farmer Portal");
+     JButton scheButton = new JButton("Programmes and Schemes");
+     JButton guiButton = new JButton("Guidelines");
+     JFrame frame = new JFrame("Helpline & Guidelines Integration");
+     
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Helpline Integration");
-            frame.setSize(300, 100);
+            Helpline H = new Helpline();
+
+            H.frame.setSize(300, 300);
 
             JPanel panel = new JPanel();
-            frame.add(panel);
+            H.frame.add(panel);
 
+            panel.add(H.callButton);
             JLabel HelplineNo = new JLabel("1800 180 1551");
-            JButton callButton = new JButton("Call Helpline");
-            panel.add(callButton);
             panel.add(HelplineNo);
             panel.add(new JLabel("Kisan Call Centre"));
-
-            callButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-
-                    String phoneNumber = "tel:18001801551";
-
-                    try {
-                        Desktop.getDesktop().browse(new URI(phoneNumber));
-                    } catch (IOException | URISyntaxException ex) {
-                        ex.printStackTrace();
-                        JOptionPane.showMessageDialog(frame, "Dev Mode: Unable to initiate the call because " + ex.getMessage());
-                    }
-                }
-            });
-            frame.setVisible(true);
+            panel.add(new JLabel("Farmer Programmes and Schemes"));
+            
+            H.callButton.addActionListener(H);
+            H.portalButton.addActionListener(H);
+            H.scheButton.addActionListener(H);
+            H.guiButton.addActionListener(H);
+            
+            panel.add(H.portalButton);
+            panel.add(H.scheButton);
+            panel.add(H.guiButton);            
+            H.frame.setVisible(true);
         });
     }
+
+    @Override
+        public void actionPerformed(ActionEvent e) {
+            String phoneNumber = "tel:18001801551";
+            String farmerPortal = "https://www.farmer.gov.in/";
+            String progandSchemes = "https://agricoop.gov.in/en/FarmWelfare";
+            String guidelines = "https://agricoop.gov.in/en/Guide";
+            String To_Be_Executed = "";
+
+            if(e.getSource()== callButton){
+                To_Be_Executed = phoneNumber;
+            }else if(e.getSource() == portalButton){
+                To_Be_Executed = farmerPortal;
+            }else if(e.getSource() == scheButton){
+                To_Be_Executed = progandSchemes;
+            }else if(e.getSource() == guiButton){
+                To_Be_Executed = guidelines;
+            }else{
+            }
+
+            try {
+                Desktop.getDesktop().browse(new URI(To_Be_Executed));
+            } catch (IOException | URISyntaxException ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(frame, "Unable to initiate request to third party browser because: "+ ex.getMessage());
+            }
+        }
+
 }

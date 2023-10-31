364db5437aa87eb80b89d2bca95e86719a595e40
NishaDev07 <143122773+NishaDev07@users.noreply.github.com>
2023-10-31 10:22:55 +0530

    Helpline.java design Update

--- Helpline.java
+++ Helpline.java
@@ -12,31 +12,59 @@ public class Helpline implements ActionListener {
      JButton portalButton = new JButton("Farmer Portal");
      JButton scheButton = new JButton("Programmes and Schemes");
      JButton guiButton = new JButton("Guidelines");
+     JLabel Label1 = new JLabel("Call Kisan Helpline");
+     JLabel Label2 = new JLabel("Extended Support");
      JFrame frame = new JFrame("Helpline & Guidelines Integration");
      
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Helpline H = new Helpline();
+            
+            Color basecolor  = new Color(87,81, 150);
+            Color fontColor = new Color(147,140,228);
 
-            H.frame.setSize(300, 300);
+            H.frame.getContentPane().setBackground(basecolor);
+            H.callButton.setBackground(Color.white);
+            H.portalButton.setBackground(Color.white);
+            H.scheButton.setBackground(Color.white);
+            H.guiButton.setBackground(Color.white);
 
-            JPanel panel = new JPanel();
-            H.frame.add(panel);
+            H.callButton.setForeground(fontColor);
+            H.portalButton.setForeground(fontColor);
+            H.scheButton.setForeground(fontColor);
+            H.guiButton.setForeground(fontColor);
+            H.Label1.setForeground(Color.white);
+            H.Label2.setForeground(Color.white);
 
-            panel.add(H.callButton);
-            JLabel HelplineNo = new JLabel("1800 180 1551");
-            panel.add(HelplineNo);
-            panel.add(new JLabel("Kisan Call Centre"));
-            panel.add(new JLabel("Farmer Programmes and Schemes"));
+            H.frame.setSize(300, 500);
+            
+            H.Label1.setFont(new Font("Roboto",Font.PLAIN,18));
+            H.Label2.setFont(new Font("Roboto",Font.PLAIN,18));
+            H.Label1.setBounds(60, 10, 200, 50);
+            H.Label2.setBounds(60,160,200,50);
+            H.callButton.setBounds(40,70,200,50);
+            H.portalButton.setBounds(40,220,200,50);
+            H.scheButton.setBounds(40,280,200,50);
+            H.guiButton.setBounds(40,340,200,50);
+
+        
+
+            H.frame.add(H.Label1);
+            H.frame.add(H.Label2);
+            H.frame.add(H.callButton);
+            H.frame.add(H.portalButton);
+            H.frame.add(H.scheButton);
+            H.frame.add(H.guiButton);
+           
             
             H.callButton.addActionListener(H);
             H.portalButton.addActionListener(H);
             H.scheButton.addActionListener(H);
             H.guiButton.addActionListener(H);
+
+            H.frame.add(H.callButton);
             
-            panel.add(H.portalButton);
-            panel.add(H.scheButton);
-            panel.add(H.guiButton);            
+            H.frame.setLayout(null);
             H.frame.setVisible(true);
         });
     }
